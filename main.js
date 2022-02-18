b43b7de269f0793f77fb5670815b81f3b696d12f
eshanajain <82397896+eshanajain@users.noreply.github.com>
2022-02-18 19:40:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 song ="";
 song1="";
 song2="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -33,5 +37,19 @@ function play()
     song.play();
 }
 
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
 
+rightWristX = results[0].pose.rightWristx;
+rightWristY = results[0].pose.rightWrist.y;
+console.log("righttWristX = " + rightWristX +" rioghtWristY = "+ rightWristY);
+
+    }
+}
 
